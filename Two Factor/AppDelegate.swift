93c217d9277a02ee1a2aa74511048c17f7ee0c80
HoMoonKi <hewenqi96@gmail.com>
2019-02-10 15:28:49 -0500

    Connect with server routes

--- Two Factor/AppDelegate.swift
+++ Two Factor/AppDelegate.swift
@@ -11,7 +11,7 @@ import Foundation
 import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
   var window: UIWindow?
   var deviceToken: String? // Testing
@@ -19,41 +19,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
     let center = UNUserNotificationCenter.current()
+    center.delegate = self
+
+    // Register notification category for customized actions
+    let confirmAction = UNNotificationAction(identifier: "CONFIRM_ACTION", title: "Confirm", options: .authenticationRequired)
+    let confirmationCategory = UNNotificationCategory(
+      identifier: "CONFIRMATION",
+      actions: [confirmAction],
+      intentIdentifiers: [],
+      options: UNNotificationCategoryOptions(rawValue: 0)
+    )
+    center.setNotificationCategories([confirmationCategory])
+    
+    // IMPORTANT: must be authorized to display alerts
     center.requestAuthorization(options: [.alert]) { (granted, err) in
       if let err = err { print(err.localizedDescription) }
       if granted { print("Authorization granted for alerts") }
-    } // IMPORTANT: must be authorized for alert to display.
+    }
     
-    UIApplication.shared.registerForRemoteNotifications() // Request devide token
+    // Request devide token
+    UIApplication.shared.registerForRemoteNotifications()
     
     return true
-  }
-
-  func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-  }
-
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  }
-
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-  }
-
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
 
   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     self.deviceToken = deviceToken.hexEncodedString()
     print("From AppDelegate: Registration success!")
+    
+    do {
+      let url = URL(string: "http://128.61.29.253:3000/ios/device-token")!
+      print(deviceToken.description)
+      let json = try JSONSerialization.data(withJSONObject: [ "token": deviceToken.hexEncodedString() ], options: [])
+      var request = URLRequest(url: url)
+      request.httpMethod = "POST"
+      request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+      request.httpBody = json
+      let task = URLSession.shared.dataTask(with: request) { (data, res, err) in
+        if err != nil { print(String(describing: err)) }
+        if let data = data { self.handleJsonData(data) }
+      }
+      task.resume()
+    } catch {
+      print(error)
+    }
+    
 //    do {
 //      let url = URL(string: "http://128.61.31.37:3000/test")!
 //      let json = try JSONSerialization.data(withJSONObject: [ "token": deviceToken.hexEncodedString()], options: [])
@@ -68,12 +78,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //    }
   }
   
-  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    print("From AppDelegate: Registration for remote notification failed!")
+  private func handleJsonData(_ data: Data) {
+    do {
+      let json = try JSONSerialization.jsonObject(with: data, options: [])
+      print(String(describing: json))
+    } catch {
+      print("Error: \(error)")
+    }
   }
   
-  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-    print("Received remote notification!")
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    print("Will present notification: \(notification.request.content.title)")
+        completionHandler(UNNotificationPresentationOptions.alert) // Display the alert anyways
+  }
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    print("Did receive response")
+    if response.actionIdentifier == "CONFIRM_ACTION" {
+      do {
+        let url = URL(string: "http://128.61.29.253:3000/confirm")!
+        let json = try JSONSerialization.data(withJSONObject: [ "username": "retarded" ], options: [])
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = json
+        let task = URLSession.shared.dataTask(with: request) { (data, res, err) in
+          if err != nil { print(String(describing: err)) }
+          if let data = data { self.handleJsonData(data) }
+        }
+        task.resume()
+      } catch {
+        print(error)
+      }
+    }
+    completionHandler()
   }
 
 }
